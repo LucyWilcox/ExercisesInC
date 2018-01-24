08c65225e0038b443fba29bedd602d515917168a
Lucy Wilcox <l.y.wilcox@gmail.com>
2018-01-23 23:01:31 -0500

    optimization

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+	int x = 5;
+	int y = x + 1;
+    printf(y);
     return 0;
 }
+
+/* It appears that in optimization, the values such as the string "Hello World"
+and the calculated value of y are stored. In the non-optimized version only the
+method to read and process that data is outlined.
+
+It's interesting that the optimized printing of "hello world" calls puts not printf
+like the non-optmized one, but the optimized printing of y calls __printf_chk so
+it looks like the optimization might also be chose better functions to use.
+
+*/
---
+++ exercises/ex01/hello.s
@@ -0,0 +1,30 @@
+	.file	"hello.c"
+	.section	.text.unlikely,"ax",@progbits
+.LCOLDB0:
+	.section	.text.startup,"ax",@progbits
+.LHOTB0:
+	.p2align 4,,15
+	.globl	main
+	.type	main, @function
+main:
+.LFB23:
+	.cfi_startproc
+	subq	$8, %rsp
+	.cfi_def_cfa_offset 16
+	movl	$6, %esi
+	movl	$1, %edi
+	xorl	%eax, %eax
+	call	__printf_chk
+	xorl	%eax, %eax
+	addq	$8, %rsp
+	.cfi_def_cfa_offset 8
+	ret
+	.cfi_endproc
+.LFE23:
+	.size	main, .-main
+	.section	.text.unlikely
+.LCOLDE0:
+	.section	.text.startup
+.LHOTE0:
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.5) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
