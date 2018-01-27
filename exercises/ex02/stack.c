509ef7ba3c6b42f8625ac31758c9d6c11926a61d
Lucy Wilcox <l.y.wilcox@gmail.com>
2018-01-27 16:32:33 -0500

    not done comments and maybe need to split off another fuction

--- exercises/ex02/stack.c
+++ exercises/ex02/stack.c
@@ -5,6 +5,22 @@ License: GNU GPLv3
 
 */
 
+/* Added solutions by Lucy Wilcox
+
+1. The program is trying to set each element of the array to 42, then make another array
+where each element is it's index, and finally print of the array generated in foo where all element are 42.
+
+2. There is a compilation error saynig that foo return the address of local variable. I think that this means
+that pointer array will not be pointing to the array generated in this function.
+
+3. I get a segmentation fault. I think it's because in when the program is trying to access the array
+in main it doesn't exist in memory at that point.
+
+4. Nothing is different... except that it doesn't print the locations 
+
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -14,7 +30,7 @@ int *foo() {
     int i;
     int array[SIZE];
 
-    //printf("%p\n", array);
+    // printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
         array[i] = 42;
@@ -26,7 +42,7 @@ void bar() {
     int i;
     int array[SIZE];
 
-    //printf("%p\n", array);
+    // printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
         array[i] = i;
