e4c858e1f2bbdc904351279b4b58d0d1ac2c7420
Lucy Wilcox <l.y.wilcox@gmail.com>
2018-02-03 15:10:28 -0500

    updates to aspace

--- exercises/ex02/aspace.c
+++ exercises/ex02/aspace.c
@@ -7,6 +7,12 @@ Address spaces sketched by Lucy Wilcox.
 
 =======
 
+4. The heap is growing up to larger addresses for p2
+
+5. The stack appears to grow down
+
+6. There is 20 bytes between each of the addresses
+
 */
 
 #include <stdio.h>
@@ -14,17 +20,32 @@ Address spaces sketched by Lucy Wilcox.
 
 int var1;
 
+void printloc () {
+	int local = 2;
+	printf("Address of local is %p\n", &local);
+}
+
 int main ()
 {
     int var2 = 5;
     void *p = malloc(128);
+    void *p2 = malloc(128);
     char *s = "Literal string";
+
+    void *r1 = malloc(23);
+    void *r2 = malloc(23);
 
     printf ("Address of main is %p\n", main);
     printf ("Address of var1 is %p\n", &var1);
     printf ("Address of var2 is %p\n", &var2);
     printf ("Address of p is %p\n", p);
+    printf("Address of p2 is %p\n", p2);
     printf ("Address of s is %p\n", s);
+
+    printloc();
+
+    printf ("Address of r1 is %p\n", r1);
+    printf("Address of r2 is %p\n", r2);
 
     return 0;
 }
