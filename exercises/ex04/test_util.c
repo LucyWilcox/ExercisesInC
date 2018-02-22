8d1e7d5f7d4fa092bdb63eaef1b6846aea7c5e22
Lucy Wilcox <l.y.wilcox@gmail.com>
2018-02-22 13:13:24 -0500

    wall not added

---
+++ exercises/ex04/test_util.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "util.h"
+#include "minunit.h"
+
+int tests_run = 0;
+
+// static char *test1() {
+//     int res = endswith("endswith", "swith");
+//     char *message = "test1 failed: endswith(\"endswith\", \"swith\") should return 1";
+//     mu_assert(message, res == 1);
+//     return NULL;
+// }
+
+
+static char *test1(){
+    char* res = icmpcode_v4(4);
+    char* message = "test1 failed: icmpcode_v4(4) should return \"fragmentation required but DF bit set\"";
+    mu_assert(message, res == "fragmentation required but DF bit set");
+    return NULL;
+}
+
+static char *test2(){
+    char* res = icmpcode_v4(23);
+    char* message = "test1 failed: icmpcode_v4(23) should return \"[unknown code]\"";
+    mu_assert(message, res == "\"[unknown code]\"");
+    return NULL;
+}
+
+static char * all_tests() {
+    mu_run_test(test1);
+    mu_run_test(test2);
+    return NULL;
+}
+
+int main(int argc, char **argv) {
+    char *result = all_tests();
+    if (result != NULL) {
+        printf("%s\n", result);
+    } else {
+        printf("ALL TESTS PASSED\n");
+    }
+    printf("Tests run: %d\n", tests_run);
+
+    return result != 0;
+}
